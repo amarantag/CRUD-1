597871a191a49875e6349016585e3a58d33fa6b4
fl3urx <fl33urx@gmail.com>
2022-10-27 21:18:29 -0500

    changed css + js

--- app.js
+++ app.js
@@ -1,2 +1,53 @@
-const nickname = document.querySelector('#inNickname');
-console.log(nickname)
+const nicknameUI = document.querySelector('#inNickname');
+const emailUI = document.querySelector('#inEmail');
+const passwordUI = document.querySelector('#inPassword');
+const btnUI = document.querySelector('#btn');
+const tableBodyUI = document.querySelector('#tableBody');
+const formularioUI = document.querySelector('#formContainer');
+
+//eventos
+btnUI.addEventListener('click', enviar);
+
+
+//funciones
+function enviar (){
+
+    let objUsuario = {
+        nickname: nicknameUI.value,
+        email: emailUI.value,
+        password: passwordUI.value,
+        id: Date.now(),
+    }
+
+    let arreglo = JSON.parse(localStorage.getItem('DB'));
+
+    if(arreglo === null){
+        arreglo = [],
+        arreglo.push(objUsuario);
+        localStorage.setItem('DB', JSON.stringify(arreglo));
+    }else{
+        arreglo.push(objUsuario);
+        localStorage.setItem('DB', JSON.stringify(arreglo));
+    }
+
+    pintar();
+
+}
+
+function pintar(){
+    let datos = JSON.parse(localStorage.getItem('DB'));
+    
+    tableBodyUI.innerHTML = ''
+
+    datos.forEach(element => {
+
+        tableBodyUI.innerHTML = `
+        <tr>
+            <td>${element.nickname} </td>
+            <td>${element.email} </td>
+            <td>${element.password} </td>
+            
+        </tr>
+        `
+    });
+}
